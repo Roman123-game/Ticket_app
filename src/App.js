822fccce7489eaa89bb816c72697f1a0c5372d78
RB <82284311+Roman123-game@users.noreply.github.com>
2021-12-29 18:18:20 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useMemo, useRef } from "react";
+import "./App.css";
+ import { v4 as uuidv4 } from "uuid";
+import Task from "./compon/Task";
 
 function App() {
+
+  const bodyInput = useRef('');
+  const [newPost, setNewPost] = useState({});
+  const [value, setValue] = useState("");
+  const [posts, setPosts] = useState([
+    {
+      id: uuidv4(),
+      listName: "Tech",
+      description: "Learn React.js",
+    },
+    {
+      id: uuidv4(),
+      listName: "Tech",
+      description: "Learn Redux",
+    },
+    {
+      id:uuidv4(),
+      listName: "Shop",
+      description: "Buy Sugar",
+    }, 
+    {
+      id:uuidv4(),
+      listName: "Shop",
+      description: "Buy Milk",
+    },
+    {
+      id: uuidv4(),
+      listName: "Rest",
+      description: "Go Piknik",
+    },
+  ]);
+  function remove( props) {
+    console.log("remove function");  
+    const afterFilter =  posts.filter(val => val.id !== props.postId);
+    console.log(afterFilter);
+    setPosts(afterFilter);
+  }
+  function Add() {
+    setNewPost({});
+    setPosts([
+      ...posts,
+      { ...newPost, id: uuidv4(), listName: value, description: bodyInput.current.value},
+    ]);
+  }
+
+  useMemo(() => {
+  return {...posts};
+  }, [posts]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Ultimate Todo
+      <div>
+        {posts.map((item) => (
+          <Task
+            remove={remove}
+            key={item.id}
+            listName={item.listName}
+            postId={item.id}
+            description={item.description}
+          ></Task>
+        ))}
+      </div>
+      <input
+        ref={bodyInput}
+        id="input"
+        className="input"
+        type="text"
+        placeholder="New Task"
+      ></input>
+      <select className="select" onChange={(event) => setValue(event.target.value)}>
+        <option value1="Shop">Shop</option>
+        <option value1="Tech">Tech</option>
+        <option value1="Rest">Rest</option>
+      </select>
+      <button onClick={Add} className="buttonAdd" type="text">
+        Add
+      </button>
     </div>
   );
 }
-
 export default App;
---
+++ src/compon/Task.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Task = (props) => {
+
+  return (
+    <div className="Task">
+      <fieldset className="fieldset">
+        <legend className="legend">{props.postId}</legend>
+        <h4 className="Title"><b>{props.listName}</b></h4>
+        <h6 className="bodyPost"><i>{props.description}</i></h6>
+          <button className="buttonRem" onClick={()=>props.remove(props)}>DeL</button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Task;
