41be7243f6084e0a035817fe074729789d4aa8a6
RB <82284311+Roman123-game@users.noreply.github.com>
2022-01-17 20:44:36 +0200

    Update Login.js

--- src/compon/Login.js
+++ src/compon/Login.js
@@ -4,7 +4,7 @@ import Button from './Button'
 const Login = (props) => {
     return (
         <div>
-        <h1>Welcome to Tickets</h1>
+        <h1>Welcome to Golden Tickets</h1>
         <h6>Press button ot login</h6>
            < input className={props.className} placeholder='User Name' ></input>
            < input  className={props.className} placeholder='Password'/>
