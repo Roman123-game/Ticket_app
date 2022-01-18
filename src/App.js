115b8c5803db1e9849ce8e0b45be7fffd183eaa2
RB <82284311+Roman123-game@users.noreply.github.com>
2022-01-18 16:48:02 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,52 @@ import React from "react";
 import "./App.css";
 import Login from "./compon/Login";
 import Posts from "./compon/Posts";
-import { BrowserRouter, Route,  Routes,} from "react-router-dom";
-import{useState} from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
   const [token, setToken] = useState(false);
-  if(!token) {
-    return(
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Login className="input" onClick={setToken} />}/>
-    </Routes>
-      </BrowserRouter>
-    )
-  }
-else
-  return (
-      <BrowserRouter>
-       <Routes>
-       <Route path ="/" element={<Posts/>} />
-       </Routes>
-      </BrowserRouter>
+  const [password, setPassword] = useState("");
 
-  );
+  function updatePassword(props, event) {
+    console.log(password);
+    setPassword(event.target.value);
+  }
+  function LoginFunc(props, event) {
+    event.stopPropagation();
+    console.log(props);
+    console.log(event);
+
+    if (password === "test1234") {
+      setToken(true);
+    } else alert("wrong password");
+  }
+  if (!token) {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Login
+                className="input"
+                value={password}
+                onClick={LoginFunc}
+                onChange={updatePassword}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+  } else {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Posts />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 export default App;
--- src/compon/Login.js
+++ src/compon/Login.js
@@ -1,16 +1,20 @@
 import React from 'react'
-
 import Button from './Button'
+
 const Login = (props) => {
+  
     return (
         <div>
         <h3>Welcome to </h3>
         <h1> Golden Tickets</h1>
         <h5>Application that helping keep all your task organized</h5>
-           < input className={props.className} placeholder='User Name' />
-           < input  className={props.className} placeholder='Password'/>
-           <Button onClick={props.onClick}/>
-           <h6> <i>*Press button to login</i></h6>
+           < input className={props.className} placeholder='User Name'/>
+           < input  className={props.className} placeholder='Password' value={props.value} 
+             onChange={(event)=>props.onChange(props,event)}/>
+           <Button onClick={(event)=>props.onClick(props,event)}/>
+           <h6> <i>
+               *UserName: any
+               *Password: test1234</i></h6>
         </div>
     )
 }
