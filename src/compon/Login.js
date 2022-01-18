f9c3f10b34bb7a3ad041c9f3a4fbe9a38345f46a
RB <82284311+Roman123-game@users.noreply.github.com>
2022-01-18 13:53:03 +0200

    update

--- src/compon/Login.js
+++ src/compon/Login.js
@@ -6,14 +6,13 @@ const Login = (props) => {
         <div>
         <h3>Welcome to </h3>
         <h1> Golden Tickets</h1>
-           < input className={props.className} placeholder='User Name' ></input>
+        <h5>Aplication that helping keep all your task organized</h5>
+           < input className={props.className} placeholder='User Name' />
            < input  className={props.className} placeholder='Password'/>
            <Button onClick={props.onClick}/>
            <h6> <i>*Press button to login</i></h6>
         </div>
     )
 }
-
-
 
 export default Login
