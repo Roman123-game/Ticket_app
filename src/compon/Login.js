aada13656be3379c8f2bc15c2026574eb69cf489
RB <82284311+Roman123-game@users.noreply.github.com>
2022-01-26 16:47:46 +0200

    update

--- src/compon/Login.js
+++ src/compon/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
         <div>
         <h3>Welcome to </h3>
         <h1> Golden Tickets</h1>
-        <h5>Application that helping keep all your task organized</h5>
+        <h5>Application that helping keep all your tasks organized</h5>
            < input className={props.className} placeholder='User Name'/>
            < input  className={props.className} placeholder='Password' value={props.value} 
              onChange={(event)=>props.onChange(props,event)}/>
