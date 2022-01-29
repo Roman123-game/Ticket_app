68ec5c587c28f9fb209307f2c3a0c8d495a21267
RB <82284311+Roman123-game@users.noreply.github.com>
2022-01-30 01:29:59 +0200

    update

--- src/compon/Select.js
+++ src/compon/Select.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { FaImage, FaRegTrashAlt} from "react-icons/fa";
+
 
 
 const Select = (props) => {
     return (
         <select className={props.className} onChange={props.onChange}>
-        <option>Shop</option>
+        <option >Shop</option>
         <option>Tech</option>
         <option>Rest</option>
       </select>
