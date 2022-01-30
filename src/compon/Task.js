10d79eac8197129d15184f35f5b87ed1929e10c4
RB <82284311+Roman123-game@users.noreply.github.com>
2022-01-30 15:55:38 +0200

    update

--- src/compon/Task.js
+++ src/compon/Task.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {FaRegTrashAlt} from "react-icons/fa";
+import {FaRegTrashAlt,FaImage} from "react-icons/fa";
 
 const Task = (props) => {
 
@@ -7,6 +7,7 @@ const Task = (props) => {
     <div className="task">
       <fieldset className="fieldset">
         <legend className="legend">{props.postId}</legend>
+        <FaImage className="image"/>
         <h4 className="title"><b>{props.listName}</b></h4>
         <h6 className="bodyPost"><i>{props.description}</i></h6>
           <button className="buttonRem" onClick={()=>props.remove(props)}><FaRegTrashAlt/></button>
