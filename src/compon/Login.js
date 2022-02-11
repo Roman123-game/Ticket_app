1bcd87fb2acd3669eb48197d89c8440781c1639d
RB <82284311+Roman123-game@users.noreply.github.com>
2022-02-11 09:48:19 +0200

    signup update

--- src/compon/Login.js
+++ src/compon/Login.js
@@ -8,8 +8,8 @@ const Login = (props) => {
         <h2>Welcome to </h2>
         <h1 className="welcomeMsg"> Golden Tickets</h1>
         <h3>Application that helping keep all your tasks organized</h3>
-           < input className={props.className} placeholder='User Name'/>
-           < input  className={props.className} placeholder='Password' value={props.value} 
+           < input className="inputLogin" placeholder='User Name'/>
+           < input className="inputLogin"  placeholder='Password' value={props.value} 
              onChange={(event)=>props.onChange(props,event)}/>
            <button className="loginBtn" onClick={(event)=>props.onClick(props,event)}>Login</button>
            <h4 className="signUpMsg"> No Account? 
