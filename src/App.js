12b0f162959d8496728a4b5cf5edfc8231c1fa78
RB <82284311+Roman123-game@users.noreply.github.com>
2022-02-25 01:58:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Login from "./compon/Login/Login";
-import Posts from "./compon/Posts/Posts";
-import { useState,useReducer } from "react";
+import Posts from "./compon/Main/Posts";
+import { useState, useReducer } from "react";
 import MainContext from "./compon/Context/MainContext";
-import {FaCartPlus} from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
 import ReducerContext from "./compon/Context/ReducerContext";
 import { v4 as uuidv4 } from "uuid";
 import InitialPosts from "./compon/Data/InitialPosts";
-
 
 function App() {
   const [token, setToken] = useState(false);
@@ -15,8 +14,9 @@ function App() {
   const [value, setValue] = useState("Shop");
   const [image, setImage] = useState(<FaCartPlus className="image" />);
   const [removeId, setRemoveId] = useState();
-
-  const [state, dispatch] = useReducer(counterReducer, { posts: InitialPosts });
+  const [state, dispatch] = useReducer(counterReducer, {
+    posts: InitialPosts,
+  });
 
   function counterReducer(state, action) {
     switch (action.type) {
@@ -33,30 +33,51 @@ function App() {
           ],
         };
       case "REMOVE_POST":
-        return { posts: state.posts.filter((itm) => itm.id !== removeId) };
+        return {
+          posts: state.posts.filter((itm) => itm.id !== removeId),
+        };
       default:
         return console.log("default");
     }
   }
   return (
-     <div>
-    {token ? 
-      (
-      <MainContext.Provider value={{ setToken,valueInput, setValueInput,value, 
-        setValue,image, setImage,removeId, setRemoveId }}>
-        <ReducerContext.Provider value={{state,dispatch}}>
-        <Posts />
-        </ReducerContext.Provider>
-      </MainContext.Provider>
-    )
-    :
-    (
-      <MainContext.Provider value={{ setToken, openSignUp, setOpenSignUp }}>
-        <Login />
-      </MainContext.Provider>
-    )
-      }
-      </div>
-  )
+    <div>
+      {" "}
+      {token ? (
+        <MainContext.Provider
+          value={{
+            setToken,
+            valueInput,
+            setValueInput,
+            value,
+            setValue,
+            image,
+            setImage,
+            removeId,
+            setRemoveId,
+          }}
+        >
+          <ReducerContext.Provider
+            value={{
+              state,
+              dispatch,
+            }}
+          >
+            <Posts />
+          </ReducerContext.Provider>{" "}
+        </MainContext.Provider>
+      ) : (
+        <MainContext.Provider
+          value={{
+            setToken,
+            openSignUp,
+            setOpenSignUp,
+          }}
+        >
+          <Login />
+        </MainContext.Provider>
+      )}{" "}
+    </div>
+  );
 }
 export default App;
---
+++ src/compon/Main/UI/Select.js
@@ -0,0 +1,12 @@
+
+const Select = (props) => {
+  return (
+    <select className={props.className} onChange={props.onChange}>
+      <option>Shop</option>
+      <option>Tech</option>
+      <option>Rest</option>
+    </select>
+  );
+};
+
+export default Select;
