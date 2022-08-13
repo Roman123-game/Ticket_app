87971aa707121e2142e2f77e7bda41c9db89734f
RB <82284311+Roman123-game@users.noreply.github.com>
2022-08-13 08:22:00 +0300

    Update UserInfo.js

--- src/compon/ModalWindows/UserInfo.js
+++ src/compon/ModalWindows/UserInfo.js
@@ -1,14 +1,17 @@
 import {FaIdCardAlt } from "react-icons/fa";
 import "./UserInfo.modal.css";
-import {memo} from "react";
+import {memo, useContext} from "react";
+import MainContext from "../Context/MainContext";
 
 
 const UserInfo = () => {
+  const {username} = useContext(MainContext);
+
   return (
     <div className="userInfo">
       <FaIdCardAlt className="imageInfo"/>
       <h2 className="user">UserInfo</h2>
-      <h4>UserName:Any</h4>
+      <h4>UserName: {username}</h4>
       <h4>Email:any@email.com</h4>
       <h4>Password:test1234</h4>
     </div>
