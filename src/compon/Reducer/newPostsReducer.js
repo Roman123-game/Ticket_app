48cadfad755a79fccf3064e7698e94ee6b7708d1
RB <82284311+Roman123-game@users.noreply.github.com>
2022-11-11 17:10:31 +0200

    using array instead object

--- src/compon/Reducer/newPostsReducer.js
+++ src/compon/Reducer/newPostsReducer.js
@@ -3,8 +3,10 @@ function newPostsReducer(newPosts, action) {
   switch (action.type) {
       case "GLOBE_POST":   
      console.log(newPosts.newPosts)
+     const postsArray = newPosts.newPosts.map(itm=>itm);
+ 
         return {
-         newPosts: newPosts.newPosts.filter((itm) => itm.description === action.payload)
+         newPosts : postsArray.filter((itm) => itm.description === action.payload)
         };
     default: 
       return console.log("default");
