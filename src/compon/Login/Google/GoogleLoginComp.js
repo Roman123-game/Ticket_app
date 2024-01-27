b4176723bc82fb6259d3f8b16bb402a7286bb3bb
RB <82284311+Roman123-game@users.noreply.github.com>
2024-01-27 20:01:32 +0200

    build(deps): update package-lock.json and GoogleLoginComp.js

--- src/compon/Login/Google/GoogleLoginComp.js
+++ src/compon/Login/Google/GoogleLoginComp.js
@@ -6,7 +6,7 @@ import MainContext from "../../Context/MainContext";
 
 
 const GoogleLoginComp = () => {
-  const clientId = "104231836749-h4u7488kujnf42obp6dmbok7omn94clu.apps.googleusercontent.com"
+  const clientId = "464357049267-2j6r2ee11mg2nl9gugn4qg7d9fn1hvj2.apps.googleusercontent.com"
 
   const onSuccess =(res) =>{
     console.log("succes login ",res.rofileObj)
